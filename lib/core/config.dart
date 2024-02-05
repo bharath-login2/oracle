665b132fea08a867dbb038ed022e2745811a3872
jithin jose <73059944+jithintbr2@users.noreply.github.com>
2024-02-05 18:07:57 +0530

    url change

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -8,7 +8,7 @@ class Config {
   static  getUrl() async {
     String? url = await Common.getSharedPref("url");
     String baseUrl;
-    String? api= Platform.isIOS?'/Mobile_app_api_ios_v4/':'/Mobile_app_api_v4/';
+    String? api= Platform.isIOS?'/Mobile_app_api_ios_v1/':'/Mobile_app_api_v1/';
     if(url!=null)
     {
       baseUrl=url.toString()+api;
