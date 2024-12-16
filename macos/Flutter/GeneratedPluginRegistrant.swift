ddf0bc0277338f0f656ee48a38942f467370bb7d
Mirza <mohammedmirsa010@gmail.com>
2024-12-16 15:12:26 +0530

    firebase configure

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,7 +19,7 @@ import path_provider_foundation
 import record_macos
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 
